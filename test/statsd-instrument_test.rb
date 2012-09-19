03adcbe9055c6e292109846e3787167ff434055d
Jesse Storimer <jstorimer@gmail.com>
2012-09-19 15:31:43 -0400

    Revert "Fix sampling tests"
    This reverts commit ddd173a4d1881ccda27b3938c1da1246c30d4f4e.

--- test/statsd-instrument_test.rb
+++ test/statsd-instrument_test.rb
@@ -161,15 +161,15 @@ class StatsDTest < Test::Unit::TestCase
 
   def test_count_with_sampling
     StatsD.unstub(:increment)
-    StatsD.stubs(:rand).returns(0.1)
+    StatsD.stubs(:rand).returns(0.6)
     StatsD.logger.expects(:info).never
 
-    StatsD.increment('sampling.foo.bar', 1, 0.6)
+    StatsD.increment('sampling.foo.bar', 1, 0.1)
   end
 
   def test_count_with_successful_sample
     StatsD.unstub(:increment)
-    StatsD.stubs(:rand).returns(0.2)
+    StatsD.stubs(:rand).returns(0.01)
     StatsD.logger.expects(:info).once.with do |string|
       string.include?('@0.1')
     end
