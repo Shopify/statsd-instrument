32b5eda03ca7c0b7069996b91da27098844952f4
Willem van Bergen <willem@vanbergen.org>
2014-04-07 12:21:00 -0400

    Use 1.8 compatible syntax

--- test/statsd_test.rb
+++ test/statsd_test.rb
@@ -34,7 +34,7 @@ class StatsDTest < Test::Unit::TestCase
   end
 
   def test_statsd_measure_executes_and_returns_block
-    StatsD.expects(:collect).with(:ms, 'values.foobar', instance_of(Float), sample_rate: 1.0)
+    StatsD.expects(:collect).with(:ms, 'values.foobar', instance_of(Float), :sample_rate => 1.0)
     return_value = StatsD.measure('values.foobar', sample_rate: 1.0) do
       'foo'
     end
