75378545e85bacd05de1f4fd3b44ddce48a8d8f3
Laurent Dang <dang.laurent@gmail.com>
2015-06-19 21:05:55 +0000

    Add RSpec custom matchers

--- lib/statsd/instrument/assertions.rb
+++ lib/statsd/instrument/assertions.rb
@@ -1,17 +1,5 @@
 module StatsD::Instrument::Assertions
-
-  def capture_statsd_calls(&block)
-    mock_backend = StatsD::Instrument::Backends::CaptureBackend.new
-    old_backend, StatsD.backend = StatsD.backend, mock_backend
-    block.call
-    mock_backend.collected_metrics
-  ensure
-    if old_backend.kind_of?(StatsD::Instrument::Backends::CaptureBackend)
-      old_backend.collected_metrics.concat(mock_backend.collected_metrics)
-    end
-
-    StatsD.backend = old_backend
-  end
+  include StatsD::Instrument::Helpers
 
   def assert_no_statsd_calls(metric_name = nil, &block)
     metrics = capture_statsd_calls(&block)
---
+++ lib/statsd/instrument/helpers.rb
@@ -0,0 +1,14 @@
+module StatsD::Instrument::Helpers
+  def capture_statsd_calls(&block)
+    mock_backend = StatsD::Instrument::Backends::CaptureBackend.new
+    old_backend, StatsD.backend = StatsD.backend, mock_backend
+    block.call
+    mock_backend.collected_metrics
+  ensure
+    if old_backend.kind_of?(StatsD::Instrument::Backends::CaptureBackend)
+      old_backend.collected_metrics.concat(mock_backend.collected_metrics)
+    end
+
+    StatsD.backend = old_backend
+  end
+end
