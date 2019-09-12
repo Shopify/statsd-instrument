e1afb0db1df987c893fac6240ec6b9e31fc3fd1f
Willem van Bergen <willem@vanbergen.org>
2019-09-12 09:52:30 -0400

    Re-add StatsD::Instument.duration.
    It was removed by mistake in an earlier version.

--- test/statsd_test.rb
+++ test/statsd_test.rb
@@ -257,6 +257,17 @@ class StatsDTest < Minitest::Test
     assert_equal 42, metric.value
   end
 
+  def test_statsd_durarion_returns_time_in_seconds
+    duration = StatsD::Instrument.duration {}
+    assert_kind_of Float, duration
+  end
+
+  def test_statsd_durarion_does_not_swallow_exceptions
+    assert_raises(RuntimeError) do
+      StatsD::Instrument.duration { raise "Foo" }
+    end
+  end
+
   protected
 
   def capture_statsd_call(&block)
