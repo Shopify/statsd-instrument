9bfecd4300fde410856b898f3f2ad93106a85cb1
Willem van Bergen <willem@vanbergen.org>
2019-09-25 08:24:51 -0400

    Add a custom Rubocop cop that checks for using `value` as keyword argument on StatsD metric method calls.

--- lib/statsd/instrument/rubocop/metric_return_value.rb
+++ lib/statsd/instrument/rubocop/metric_return_value.rb
@@ -4,9 +4,10 @@ module RuboCop
   module Cop
     module StatsD
       # This Rubocop will check for using the return value of StatsD metric calls, which is deprecated.
-      # To run, use the following command:
+      # To check your codebase, use the following Rubocop invocation:
       #
-      #     rubocop --require /absolute/path/to/metric_return_value.rb --only StatsD/MetricReturnValue filename
+      #     rubocop --require `bundle show statsd-instrument`/lib/statsd/instrument/rubocop//metric_return_value.rb \
+      #       --only StatsD/MetricReturnValue
       #
       # This cop cannot autocorrect offenses. In production code, StatsD should be used in a fire-and-forget
       # fashion. This means that you shouldn't rely on the return value. If you really need to access the
