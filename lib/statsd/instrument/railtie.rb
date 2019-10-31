7e6e79162c1fc5cf7e86af8f88525e9241d1f4ae
Willem van Bergen <willem@vanbergen.org>
2019-10-31 06:29:57 -0400

    Remove setting a backend in the Railtie.

--- lib/statsd/instrument/railtie.rb
+++ lib/statsd/instrument/railtie.rb
@@ -8,8 +8,4 @@ class StatsD::Instrument::Railtie < Rails::Railtie
   initializer 'statsd-instrument.use_rails_logger' do
     ::StatsD.logger = Rails.logger
   end
-
-  initializer 'statsd-instrument.setup_backend', after: 'statsd-instrument.use_rails_logger' do
-    ::StatsD.backend = ::StatsD::Instrument::Environment.default_backend
-  end
 end
