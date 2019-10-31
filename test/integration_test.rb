d55ed871d15c19216cefa8b464058ee073204eb6
Willem van Bergen <willem@vanbergen.org>
2019-10-31 06:30:25 -0400

    Rewrite integration test to be built on top of new client.

--- test/integration_test.rb
+++ test/integration_test.rb
@@ -6,39 +6,24 @@ class IntegrationTest < Minitest::Test
   def setup
     @server = UDPSocket.new
     @server.bind('localhost', 0)
-    port = @server.addr[1]
 
-    @old_backend = StatsD.backend
-    StatsD.backend = StatsD::Instrument::Backends::UDPBackend.new("localhost:#{port}")
+    @env = StatsD::Instrument::Environment.new(
+      'STATSD_ADDR' => "#{@server.addr[2]}:#{@server.addr[1]}",
+      'STATSD_IMPLEMENTATION' => 'dogstatsd',
+      'STATSD_ENV' => 'production',
+    )
+
+    @old_client = StatsD.singleton_client
+    StatsD.singleton_client = @env.client
   end
 
   def teardown
+    StatsD.singleton_client = @old_client
     @server.close
-    StatsD.backend = @old_backend
   end
 
   def test_live_local_udp_socket
     StatsD.increment('counter')
     assert_equal "counter:1|c", @server.recvfrom(100).first
-  end
-
-  def test_synchronize_in_exit_handler_handles_thread_error_and_exits_cleanly
-    pid = fork do
-      Signal.trap('TERM') do
-        StatsD.increment('exiting')
-        Process.exit!(0)
-      end
-
-      sleep 100
-    end
-
-    Process.kill('TERM', pid)
-    _, exit_status = Process.waitpid2(pid)
-
-    assert_equal 0, exit_status, "The forked process did not exit cleanly"
-    assert_equal "exiting:1|c", @server.recvfrom_nonblock(100).first
-
-  rescue NotImplementedError
-    pass("Fork is not implemented on #{RUBY_PLATFORM}")
   end
 end
