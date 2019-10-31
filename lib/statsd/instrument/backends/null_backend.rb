6538af928c9908406fda2897c37abc253b3df011
Willem van Bergen <willem@vanbergen.org>
2019-10-31 06:34:17 -0400

    Remove files and tests related to legacy client.

--- lib/statsd/instrument/backends/null_backend.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-module StatsD::Instrument::Backends
-  # The null backend does nothing when receiving a metric, effectively disabling the gem completely.
-  class NullBackend < StatsD::Instrument::Backend
-    def collect_metric(metric)
-    end
-  end
-end
