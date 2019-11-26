433bdad8fbc54184380e673c3e2b22b531211db7
Willem van Bergen <willem@vanbergen.org>
2019-11-26 13:39:28 -0500

    Updated Yardoc for StatsD and StatsD::Instrument::Client.

--- lib/statsd/instrument/client.rb
+++ lib/statsd/instrument/client.rb
@@ -1,12 +1,27 @@
 # frozen_string_literal: true
 
-# The Client is the main interface for using StatsD.
+# The Client is the main interface for using StatsD. It defines the metric
+# methods that you would normally call from your application.
 #
-# @note This new Client implementation is intended to become the new default in the
-#   next major release of this library. While this class may already be functional,
-#   we provide no guarantees about the API and the behavior may change.
+# The client set to {StatsD.singleton_client} will handle all metric calls made
+# against the StatsD singleton, e.g. `StatsD.increment`.
+#
+# We recommend that the configuration of the StatsD setup is provided through
+# environment variables
+#
+# You are encouraged to instantiate multiple clients, and instantiate variants
+# of an existing clients using {#clone_with_options}. We recommend instiating
+# a separate client for every logical component of your application using
+# `clone_with_options`, and setting a different metric `prefix`.
+#
+# @see StatsD.singleton_client
+# @see #clone_with_options
 class StatsD::Instrument::Client
   class << self
+    # Instantiates a StatsD::Instrument::Client using configuration values provided in
+    # environment variables.
+    #
+    # @see StatsD::Instrument::Environment
     def from_env(
       env = StatsD::Instrument::Environment.current,
       prefix: env.statsd_prefix,
@@ -26,7 +41,14 @@ class StatsD::Instrument::Client
       )
     end
 
+    # Finds the right DatagramBuilder class for a given implementation.
     # @private
+    # @param [Symbol, String] implementation The name of the implementation, e.g.
+    #   `"statsd"` or `:datadog`.
+    # @return [Class] The subclass of {StatsD::Instrument::DatagramBuilder}
+    #   builder to use to generate UDP datagrams for the given implementation.
+    # @raise `NotImplementedError` if the implementation is not recognized or
+    #   supported.
     def datagram_builder_class_for_implementation(implementation)
       case implementation.to_s
       when 'statsd'
@@ -39,8 +61,87 @@ class StatsD::Instrument::Client
     end
   end
 
-  attr_reader :sink, :datagram_builder_class, :prefix, :default_tags, :default_sample_rate
+  # The class to use to build StatsD datagrams. The build the actual datagrams,
+  # the class will be instantiated, potentially multiple times, by the client.
+  #
+  # @return [Class] A subclass of {StatsD::Instrument::DatagramBuilder}
+  # @see .datagram_builder_class_for_implementation
+  attr_reader :datagram_builder_class
 
+  # The sink to send UDP datagrams to.
+  #
+  # This can be set to any object that responds to the following methods:
+  #
+  # - `sample?` which should return true if the metric should sampled, i.e.
+  #   actually sent to the sink.
+  # - `#<<` which takes a UDP datagram as string to emit the datagram. This
+  #   method will only be called if `sample?` returned `true`.
+  #
+  # Generally, you should use an instance of one of the following classes that
+  # ship with this library:
+  #
+  # - {StatsD::Instrument::UDPSink} A sink that will actually emit the provided
+  #   datagrams over UDP.
+  # - {StatsD::Instrument::NullSink} A sink that will simply swallow every
+  #   datagram. This sink is when testing your application.
+  # - {StatsD::Instrument::LogSink} A sink that log all provided datagrams to
+  #   a Logger, normally {StatsD.logger}.
+  #
+  # @return [#sample?, #<<]
+  attr_reader :sink
+
+  # The prefix to prepend to the metric names that are emitted through this
+  # client, using a dot (`.`) as namespace separator. E.g. when the prefix is
+  # set to `foo`, and you emit a metric named `bar`, the metric name will be
+  # `foo.bar`.
+  #
+  # Generally all the metreics you emit to the same StatsD server will share a
+  # single, global namespace. If you are emitting metrics from multiple
+  # applications, using a prefix is recommended to prevent metric name
+  # collisions.
+  #
+  # You can also leave this value to be `nil` if you don't want to prefix your
+  # metric names.
+  #
+  # @return [String, nil]
+  #
+  # @note The `prefix` can be overriden by any metric call by setting the
+  #   `no_prefix` keyword argument to `true`. We recommend against doing this,
+  #   but this behavior is retained for backwards compatibility.
+  #   Rather, when you feel the need to do this, we recommend instantiating
+  #   a new client without prefix (using {#clone_with_options}), and using it
+  #   to emit the metric.
+  attr_reader :prefix
+
+  # The tags to apply to all the metrics emitted through this client.
+  #
+  # The tags can be supplied in normal form: an array of strings. You can also
+  # provide a hash, which will be turned into normal form by concatanting the
+  # key and the value using a colon. To not use any default tags, set to `nil`.
+  # Note that other components of your StatsD metric pipeline may also add tags
+  # to metrics. E.g. the DataDog agent may add add tags like `hostname`.
+  #
+  # We generally recommend to not use default tags, or use them sparingly.
+  # Adding tags to every metric easily introduces carninality explosions, which
+  # will make metrics less precise due to the lossy nature of aggregation. It
+  # also makes your infrastructure more expsnive to run, and the user interface
+  # of your metric explorer less responsive.
+  #
+  # @return [Array<String>, Hash, nil]
+  attr_reader :default_tags
+
+  # The default sample rate to use for metrics that are emitted without a
+  # sample rate set. This should be a value between 0 (never emit a metric) and
+  # 1.0 (always emit). If it is not set, the default value 1.0 is used.
+  #
+  # We generally recommend setting sample rates on individual metrics based
+  # on their frequency, rather than changing the default sample rate.
+  #
+  # @return [Float] (default: 1.0) A value between 0.0 and 1.0.
+  attr_reader :default_sample_rate
+
+  # Instantiates a new client.
+  # @see .from_env to instantiate a client using environment variables.
   def initialize(
     prefix: nil,
     default_sample_rate: 1.0,
@@ -216,18 +317,15 @@ class StatsD::Instrument::Client
     end
   end
 
-  # Emits a service check.
+  # Emits a service check. Services Checks allow you to characterize the status
+  # of a service in order to monitor it within Datadog.
   #
-  # @param [String] title Event title.
-  # @param [String] text Event description. Newlines are allowed.
-  # @param [Time] timestamp The of the event. If not provided,
-  #   Datadog will interpret it as the current timestamp.
-  # @param [String] hostname A hostname to associate with the event.
-  # @param [String] aggregation_key An aggregation key to group events with the same key.
-  # @param [String] priority Priority of the event. Either "normal" (default) or "low".
-  # @param [String] source_type_name The source type of the event.
-  # @param [String] alert_type Either "error", "warning", "info" (default) or "success".
-  # @param [Array, Hash] tags Tags to associate with the event.
+  # @param name (see StatsD::Instrument::DogStatsDDatagramBuilder#_sc)
+  # @param status (see StatsD::Instrument::DogStatsDDatagramBuilder#_sc)
+  # @param timestamp (see StatsD::Instrument::DogStatsDDatagramBuilder#_sc)
+  # @param hostname (see StatsD::Instrument::DogStatsDDatagramBuilder#_sc)
+  # @param tags (see StatsD::Instrument::DogStatsDDatagramBuilder#_sc)
+  # @param message (see StatsD::Instrument::DogStatsDDatagramBuilder#_sc)
   # @return [void]
   #
   # @note Supported by the Datadog implementation only.
@@ -236,15 +334,17 @@ class StatsD::Instrument::Client
       timestamp: timestamp, hostname: hostname, tags: tags, message: message))
   end
 
-  # Emits an event.
+  # Emits an event. An event represents any record of activity noteworthy for engineers.
   #
-  # @param [String] name Name of the service
-  # @param [Symbol] status Either `:ok`, `:warning`, `:critical` or `:unknown`
-  # @param [Time] timestamp The moment when the service was checked. If not provided,
-  #   Datadog will interpret it as the current timestamp.
-  # @param [String] hostname A hostname to associate with the check.
-  # @param [Array, Hash] tags Tags to associate with the check.
-  # @param [String] message A message describing the current state of the service check.
+  # @param title (see StatsD::Instrument::DogStatsDDatagramBuilder#_e)
+  # @param text (see StatsD::Instrument::DogStatsDDatagramBuilder#_e)
+  # @param timestamp (see StatsD::Instrument::DogStatsDDatagramBuilder#_e)
+  # @param hostname (see StatsD::Instrument::DogStatsDDatagramBuilder#_e)
+  # @param aggregation_key (see StatsD::Instrument::DogStatsDDatagramBuilder#_e)
+  # @param priority (see StatsD::Instrument::DogStatsDDatagramBuilder#_e)
+  # @param source_type_name (see StatsD::Instrument::DogStatsDDatagramBuilder#_e)
+  # @param alert_type (see StatsD::Instrument::DogStatsDDatagramBuilder#_e)
+  # @param tags (see StatsD::Instrument::DogStatsDDatagramBuilder#_e)
   # @return [void]
   #
   # @note Supported by the Datadog implementation only.
