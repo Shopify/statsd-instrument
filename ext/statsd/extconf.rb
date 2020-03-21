ddfce73006155f13e10e4cdfcdea13bd571a9cce
Lourens Naudé <lourens@bearmetal.eu>
2020-03-21 19:34:41 +0000

    Enable warnings + pedantic flag and allow for easier debugging by setting ENV var STATSD_EXT_DEBUG

--- ext/statsd/extconf.rb
+++ ext/statsd/extconf.rb
@@ -1,5 +1,12 @@
 require 'mkmf'
 
-$CFLAGS = "-O3"
+append_cflags '-pedantic'
+append_cflags '-Wall'
+if ENV['STATSD_EXT_DEBUG']
+  append_cflags "-Og"
+  append_cflags '-ggdb3'
+else
+  append_cflags "-O3"
+end
 
 create_makefile('statsd/statsd')
