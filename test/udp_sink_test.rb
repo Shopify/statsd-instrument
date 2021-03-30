89472ff4ce65e071274ee204e36bebaf12b7331a
Jean Boussier <jean.boussier@gmail.com>
2021-03-30 22:21:52 +0200

    Refactor the ThreadedUDPSink and fix a few bugs

--- test/udp_sink_test.rb
+++ test/udp_sink_test.rb
@@ -2,20 +2,9 @@
 
 require "test_helper"
 
-class UDPSinkTest < Minitest::Test
-  def setup
-    @receiver = UDPSocket.new
-    @receiver.bind("localhost", 0)
-    @host = @receiver.addr[2]
-    @port = @receiver.addr[1]
-  end
-
-  def teardown
-    @receiver.close
-  end
-
+module SharedUDPSinkTests
   def test_udp_sink_sends_data_over_udp
-    udp_sink = StatsD::Instrument::UDPSink.new(@host, @port)
+    udp_sink = @sink_class.new(@host, @port)
     udp_sink << "foo:1|c"
 
     datagram, _source = @receiver.recvfrom(100)
@@ -23,7 +12,7 @@ class UDPSinkTest < Minitest::Test
   end
 
   def test_sample?
-    udp_sink = StatsD::Instrument::UDPSink.new(@host, @port)
+    udp_sink = @sink_class.new(@host, @port)
     assert(udp_sink.sample?(1))
     refute(udp_sink.sample?(0))
 
@@ -35,7 +24,7 @@ class UDPSinkTest < Minitest::Test
   end
 
   def test_parallelism
-    udp_sink = StatsD::Instrument::UDPSink.new(@host, @port)
+    udp_sink = @sink_class.new(@host, @port)
     50.times { |i| Thread.new { udp_sink << "foo:#{i}|c" << "bar:#{i}|c" } }
     datagrams = []
     100.times do
@@ -50,12 +39,13 @@ class UDPSinkTest < Minitest::Test
     UDPSocket.stubs(:new).returns(socket = mock("socket"))
 
     seq = sequence("connect_fail_connect_succeed")
+    socket.stubs(:flush)
     socket.expects(:connect).with("localhost", 8125).in_sequence(seq)
     socket.expects(:send).raises(Errno::EDESTADDRREQ).in_sequence(seq)
     socket.expects(:connect).with("localhost", 8125).in_sequence(seq)
     socket.expects(:send).returns(1).in_sequence(seq)
 
-    udp_sink = StatsD::Instrument::UDPSink.new("localhost", 8125)
+    udp_sink = @sink_class.new("localhost", 8125)
     udp_sink << "foo:1|c"
     udp_sink << "bar:1|c"
     # Let the dispatcher thread emit
@@ -63,23 +53,62 @@ class UDPSinkTest < Minitest::Test
   end
 
   def test_sends_datagram_in_signal_handler
-    udp_sink = StatsD::Instrument::UDPSink.new(@host, @port)
+    udp_sink = @sink_class.new(@host, @port)
     pid = fork do
+      udp_sink.after_fork if udp_sink.respond_to?(:after_fork)
+
       Signal.trap("TERM") do
         udp_sink << "exiting:1|c"
-        Process.exit!(0)
+        # FIXME: Somehow the TheeadedUDPSink does write the message (I verified it, Socket#send is called and returns)
+        # However somehow, without that extremly small sleep the packet is never recieved by the parent.
+        sleep 0.001
+        Process.exit(0)
       end
 
       sleep(10)
     end
 
+    sleep 0.3 # We need to give the subprocess the time to setup the signal handler etc.
     Process.kill("TERM", pid)
-    _, exit_status = Process.waitpid2(pid)
 
-    assert_equal(0, exit_status, "The forked process did not exit cleanly")
+    assert_equal(@receiver, @receiver.wait_readable(2), "No packet to read after 2 seconds")
     assert_equal("exiting:1|c", @receiver.recvfrom_nonblock(100).first)
 
+    _, exit_status = Process.waitpid2(pid)
+    assert_equal(0, exit_status, "The forked process did not exit cleanly")
   rescue NotImplementedError
     pass("Fork is not implemented on #{RUBY_PLATFORM}")
+  end
+end
+
+class UDPSinkTest < Minitest::Test
+  include SharedUDPSinkTests
+
+  def setup
+    @sink_class = StatsD::Instrument::UDPSink
+    @receiver = UDPSocket.new
+    @receiver.bind("localhost", 0)
+    @host = @receiver.addr[2]
+    @port = @receiver.addr[1]
+  end
+
+  def teardown
+    @receiver.close
+  end
+end
+
+class ThreadedUDPSinkTest < Minitest::Test
+  include SharedUDPSinkTests
+
+  def setup
+    @sink_class = StatsD::Instrument::ThreadedUDPSink
+    @receiver = UDPSocket.new
+    @receiver.bind("localhost", 0)
+    @host = @receiver.addr[2]
+    @port = @receiver.addr[1]
+  end
+
+  def teardown
+    @receiver.close
   end
 end
