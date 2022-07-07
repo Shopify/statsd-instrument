fe5a593e494b8b94c1e92717e98638f7b4f87d41
Jean Boussier <jean.boussier@gmail.com>
2022-07-07 13:24:54 +0200

    Wake up the dispatch thread once the buffer is past a threshold
    The current sleep loop can lead to large memory usage if lots of
    events are queued rapidly. In such scenario the buffer can grow
    very large until the threads wake up and flush it.
    
    To avoid this, after enqueing a message, if the buffer reached
    a threshold we explictly wake up the dispatch thread to trigger
    a flush.

--- lib/statsd/instrument/batched_udp_sink.rb
+++ lib/statsd/instrument/batched_udp_sink.rb
@@ -6,11 +6,13 @@ module StatsD
     #   to become the new default in the next major release of this library.
     class BatchedUDPSink
       DEFAULT_FLUSH_INTERVAL = 1.0
+      DEFAULT_THREAD_PRIORITY = 100
+      DEFAULT_FLUSH_THRESHOLD = 50
       MAX_PACKET_SIZE = 508
 
-      def self.for_addr(addr, flush_interval: DEFAULT_FLUSH_INTERVAL)
+      def self.for_addr(addr, **kwargs)
         host, port_as_string = addr.split(":", 2)
-        new(host, Integer(port_as_string), flush_interval: flush_interval)
+        new(host, Integer(port_as_string), **kwargs)
       end
 
       attr_reader :host, :port
@@ -21,10 +23,10 @@ module StatsD
         end
       end
 
-      def initialize(host, port, flush_interval: DEFAULT_FLUSH_INTERVAL)
+      def initialize(host, port, flush_interval: DEFAULT_FLUSH_INTERVAL, thread_priority: DEFAULT_THREAD_PRIORITY, flush_threshold: DEFAULT_FLUSH_THRESHOLD)
         @host = host
         @port = port
-        @dispatcher = Dispatcher.new(host, port, flush_interval)
+        @dispatcher = Dispatcher.new(host, port, flush_interval, flush_threshold, thread_priority)
         ObjectSpace.define_finalizer(self, self.class.finalize(@dispatcher))
       end
 
@@ -35,6 +37,10 @@ module StatsD
       def <<(datagram)
         @dispatcher << datagram
         self
+      end
+
+      def shutdown(*args)
+        @dispatcher.shutdown(*args)
       end
 
       class Dispatcher
@@ -50,33 +56,33 @@ module StatsD
           Concurrent::Array
         end
 
-        def initialize(host, port, flush_interval)
+        def initialize(host, port, flush_interval, flush_threshold, thread_priority)
           @host = host
           @port = port
           @interrupted = false
           @flush_interval = flush_interval
+          @flush_threshold = flush_threshold
+          @thread_priority = thread_priority
           @buffer = BUFFER_CLASS.new
           @dispatcher_thread = Thread.new { dispatch }
+          @pid = Process.pid
+          @monitor = Monitor.new
+          @condition = @monitor.new_cond
         end
 
         def <<(datagram)
-          unless @dispatcher_thread&.alive?
-            # If the dispatcher thread is dead, we assume it is because
-            # the process was forked. So to avoid ending datagrams twice
-            # we clear the buffer.
-            # However if the main the main thread is dead, we won't be able
-            # to spawn a new thread, so we fallback to sending our datagram directly.
-            if Thread.main.alive?
-              @buffer.clear
-              @dispatcher_thread = Thread.new { dispatch }
-            else
-              @buffer << datagram
-              flush
-              return self
+          if thread_healthcheck
+            @buffer << datagram
+
+            # To avoid sending too many signals when the thread is already flushing
+            # We only signal when the queue size is a multiple of `flush_threshold`
+            if @buffer.size % @flush_threshold == 0
+              wakeup_thread
             end
+          else
+            flush
           end
 
-          @buffer << datagram
           self
         end
 
@@ -91,6 +97,24 @@ module StatsD
 
         private
 
+        def wakeup_thread
+          begin
+            @monitor.synchronize do
+              @condition.signal
+            end
+          rescue ThreadError
+            # Can't synchronize from trap context
+            Thread.new { wakeup_thread }.join
+            return
+          end
+
+          begin
+            @dispatcher_thread&.run
+          rescue ThreadError # Somehow the thread just died
+            thread_healthcheck
+          end
+        end
+
         NEWLINE = "\n".b.freeze
         def flush
           return if @buffer.empty?
@@ -98,7 +122,7 @@ module StatsD
           datagrams = @buffer.shift(@buffer.size)
 
           until datagrams.empty?
-            packet = String.new(datagrams.pop, encoding: Encoding::BINARY, capacity: MAX_PACKET_SIZE)
+            packet = String.new(datagrams.shift, encoding: Encoding::BINARY, capacity: MAX_PACKET_SIZE)
 
             until datagrams.empty? || packet.bytesize + datagrams.first.bytesize + 1 > MAX_PACKET_SIZE
               packet << NEWLINE << datagrams.shift
@@ -108,6 +132,29 @@ module StatsD
           end
         end
 
+        def thread_healthcheck
+          # TODO: We have a race condition on JRuby / Truffle here. It could cause multiple
+          # dispatcher threads to be spawned, which would cause problems.
+          # However we can't simply lock here as we might be called from a trap context.
+          unless @dispatcher_thread&.alive?
+            # If the main the main thread is dead the VM is shutting down so we won't be able
+            # to spawn a new thread, so we fallback to sending our datagram directly.
+            return false unless Thread.main.alive?
+
+            # If the dispatcher thread is dead, it might be because the process was forked.
+            # So to avoid sending datagrams twice we clear the buffer.
+            if @pid != Process.pid
+              StatsD.logger.info { "[#{self.class.name}] Restarting the dispatcher thread after fork" }
+              @pid = Process.pid
+              @buffer.clear
+            else
+              StatsD.logger.info { "[#{self.class.name}] Restarting the dispatcher thread" }
+            end
+            @dispatcher_thread = Thread.new { dispatch }.tap { |t| t.priority = @thread_priority }
+          end
+          true
+        end
+
         def dispatch
           until @interrupted
             begin
@@ -115,7 +162,11 @@ module StatsD
               flush
               next_sleep_duration = @flush_interval - (Process.clock_gettime(Process::CLOCK_MONOTONIC) - start)
 
-              sleep(next_sleep_duration) if next_sleep_duration > 0
+              if next_sleep_duration > 0
+                @monitor.synchronize do
+                  @condition.wait(next_sleep_duration)
+                end
+              end
             rescue => error
               report_error(error)
             end
