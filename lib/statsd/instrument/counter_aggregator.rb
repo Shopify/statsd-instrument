f512da77ed9a607fd5afa0384a48fd84cff5c308
Pedro Tanaka <pedro.tanaka@shopify.com>
2024-06-06 18:19:03 +0200

    simple test
    Signed-off-by: Pedro Tanaka <pedro.tanaka@shopify.com>

--- lib/statsd/instrument/counter_aggregator.rb
+++ lib/statsd/instrument/counter_aggregator.rb
@@ -1,42 +1,57 @@
 # frozen_string_literal: true
 
-class CounterAggregator
-  def initialize(sink)
-    @sink = sink
-    @counters = {}
-  end
-
-  def increment(name, value = 1, sample_rate: 1.0, tags: nil)
-    key = packet_key(name, tags)
-    if @counters.has_key?(key)
-      if sample_rate < 1.0
-        value = (value.to_f / sample_rate).round.to_i
+module StatsD
+  module Instrument
+    class CounterAggregator
+      def initialize(sink)
+        @sink = sink
+        @counters = {}
       end
-      @counters[key][:value] += value
-    else
-      @counters[key] = {
-        name: name,
-        value: value,
-        tags: tags,
-      }
-    end
-    @counters[key] ||= {
-      name: name,
-      tags: tags,
-    }
-  end
 
-  def flush
+      def increment(name, value = 1, sample_rate: 1.0, tags: nil)
+        tags ||= []
+        tags = tags_sorted(tags)
+        key = packet_key(name, tags)
+        if sample_rate < 1.0
+          value = (value.to_f / sample_rate).round.to_i
+        end
+        if @counters.has_key?(key)
 
-  end
+          @counters[key][:value] += value
+        else
+          @counters[key] = {
+            name: name,
+            value: value,
+            tags: tags,
+          }
+        end
+      end
 
-  private
-  def packet_key(name, tags)
-    if tags.is_a?(Hash)
-      "#{name}#{tags.sort_by { |k, v| k.to_s }.to_s}"
-    else
-      tags.sort!
-      "#{name}#{tags.join('')}"
+      def flush
+        @counters.each do |key, counter|
+          @sink << build_datagram(counter)
+        end
+        @counters.clear
+      end
+
+      private
+
+      def packet_key(name, tags = [])
+        "#{name}#{tags.join('')}"
+      end
+
+      def build_datagram(counter)
+        "#{counter[:name]}:#{counter[:value]}|c|##{counter[:tags].join(',')}"
+      end
+
+      def tags_sorted(tags)
+        if tags.is_a?(Hash)
+          tags.sort_by { |k, v| k.to_s }
+          tags.map { |k, v| "#{k}:#{v}" }
+        else
+          tags.sort
+        end
+      end
     end
   end
 end
