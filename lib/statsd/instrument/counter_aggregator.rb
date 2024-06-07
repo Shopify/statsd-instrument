dbe11046de05149c72dcedf562775e7d4c7de782
Pedro Tanaka <pedro.tanaka@shopify.com>
2024-06-07 09:32:31 +0200

    Improving allocations
    Signed-off-by: Pedro Tanaka <pedro.tanaka@shopify.com>

--- lib/statsd/instrument/counter_aggregator.rb
+++ lib/statsd/instrument/counter_aggregator.rb
@@ -24,15 +24,13 @@ module StatsD
       # @param tags [Hash{String, Symbol => String},Array<String>] The tags to attach to the counter.
       # @param no_prefix [Boolean] If true, the metric will not be prefixed.
       # @return [void]
-      def increment(name, value = 1, sample_rate: 1.0, tags: nil, no_prefix: false)
-        tags ||= []
+      def increment(name, value = 1, sample_rate: 1.0, tags: [], no_prefix: false)
         tags = tags_sorted(tags)
         key = packet_key(name, tags, no_prefix)
         if sample_rate < 1.0
           value = (value.to_f / sample_rate).round.to_i
         end
         if @counters.key?(key)
-
           @counters[key][:value] += value
         else
           @counters[key] = {
@@ -58,8 +56,20 @@ module StatsD
 
       private
 
+      def tags_sorted(tags)
+        return [].freeze if tags.nil? || tags.empty?
+
+        if tags.is_a?(Hash)
+          tags.sort_by! { |k, _v| k.to_s }
+          tags.map! { |k, v| "#{k}:#{v}" }
+        else
+          tags.sort!
+        end
+        tags
+      end
+
       def packet_key(name, tags = [], no_prefix = false)
-        "".b << name.b << tags.join("").b << no_prefix.to_s.b
+        "#{name}#{tags.join}#{no_prefix}".b
       end
 
       def datagram_builder(no_prefix:)
@@ -67,15 +77,6 @@ module StatsD
           prefix: no_prefix ? nil : @metric_prefix,
           default_tags: @default_tags,
         )
-      end
-
-      def tags_sorted(tags)
-        if tags.is_a?(Hash)
-          tags.sort_by { |k, _v| k.to_s }
-          tags.map { |k, v| "#{k}:#{v}" }
-        else
-          tags.sort
-        end
       end
     end
   end
