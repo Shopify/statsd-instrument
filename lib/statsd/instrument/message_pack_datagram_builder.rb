fb1af0c3a66bcf5a674294b5c80d30f14b679b39
Andrew Hayworth <andrew.hayworth@shopify.com>
2024-06-26 17:03:40 -0400

    Add basic, single-message messagepack datagram builder
    This commit will serialize a statsd datagram as a custom messagepack
    extension type (and many details around this type are TBD; and not all
    datagram types are actually supported).
    
    For now, it'll just serialize one datagram into a single messagepack
    message. We are setting `values` as an array (in the hopes of value
    packing), but this commit doesn't implement that.
    
    We also can't reliably serialize multiple of these calls into a batched
    packet, so you'd want to use the regular `UDPSink` with this:
    
    ```
    require 'statsd/instrument'
    client = StatsD.singleton_client.clone_with_options(
       sink: StatsD::Instrument::UDPSink.new(host, port),
       datagram_builder_class: StatsD::Instrument::MessagePackDatagramBuilder,
    )
    
    client.increment("foo", 1) # or whatever
    ```
    
    This is just something of a starting point, and this particular commit
    will probably not merge into main as-is.

---
+++ lib/statsd/instrument/message_pack_datagram_builder.rb
@@ -0,0 +1,164 @@
+# frozen_string_literal: true
+
+require "msgpack"
+
+module StatsD
+  module Instrument
+    MsgPackDatagram = Struct.new(:name, :values, :metric_type, :sample_rate, :labels)
+
+    # @note This class is part of the new Client implementation that is intended
+    #   to become the new default in the next major release of this library.
+    class MessagePackDatagramBuilder < StatsD::Instrument::DogStatsDDatagramBuilder
+      unsupported_datagram_types :kv
+
+      class << self
+        def datagram_class
+          StatsD::Instrument::DogStatsDDatagram
+        end
+      end
+
+      def initialize(prefix: nil, default_tags: nil)
+        # @prefix = prefix.nil? ? "" : "#{prefix}.".tr(":|@", "_")
+        # @default_tags = if default_tags.nil? || default_tags.empty?
+        #                   nil
+        #                 elsif default_tags.is_a?(Hash)
+        #                   default_tags
+        #                     .transform_values { |v| /[|,]/.match?(key) ? v.tr("|,", "") : v }
+        #                 else
+        #                   default_tags
+        #                     .map { |t| t.split(":", 2).tr("|,", "") }
+        #                     .reject { |(k, v)| v.nil? }
+        #                     .to_h
+        #                 end
+
+        MessagePack::DefaultFactory.register_type(
+          0x06,
+          MsgPackDatagram,
+          packer: -> (datagram, packer) {
+            packer.write datagram.name
+            packer.write datagram.values
+
+            case datagram.metric_type
+            when :g
+              packer.write 0
+            when :c
+              packer.write 1
+            when :d
+              packer.write 2
+            when :h
+              packer.write 3
+            when :ms
+              packer.write 4
+            when :s
+              packer.write 5
+            when :e
+              packer.write 6
+            when :sc
+              packer.write 7
+            when :kv
+              packer.write 8
+            end
+
+            packer.write datagram.sample_rate
+            packer.write datagram.labels
+          },
+          unpacker: -> (unpacker) {
+            datagram = MsgPackDatagram.new(
+              name: packer.read,
+              values: packer.read,
+            )
+
+            case packer.read
+            when 0
+              datagram.metric_type = :g
+            when 1
+              datagram.metric_type = :c
+            when 2
+              datagram.metric_type = :d
+            when 3
+              datagram.metric_type = :h
+            when 4
+              datagram.metric_type = :ms
+            when 5
+              datagram.metric_type = :s
+            when 6
+              datagram.metric_type = :e
+            when 7
+              datagram.metric_type = :sc
+            when 8
+              datagram.metric_type = :kv
+            end
+
+            datagram.sample_rate = packer.read
+            datagram.labels = packer.read
+          },
+          recursive: true
+        )
+
+        super
+      end
+
+      def latency_metric_type
+        :d
+      end
+
+      def generate_generic_datagram(name, value, type, sample_rate, tags)
+        tag_string = "".b
+        unless @default_tags.nil?
+          tag_string << @default_tags << ","
+        end
+        compile_tags(tags, tag_string) unless tags.nil?
+
+        MessagePack::DefaultFactory.dump(
+          MsgPackDatagram.new(
+            name: name,
+            values: [value],
+            metric_type: type.to_sym,
+            sample_rate: sample_rate,
+            labels: tag_string,
+          )
+        )
+      end
+
+      # Constructs an event datagram.
+      #
+      # @param [String] title Event title.
+      # @param [String] text Event description. Newlines are allowed.
+      # @param [Time] timestamp The of the event. If not provided,
+      #   Datadog will interpret it as the current timestamp.
+      # @param [String] hostname A hostname to associate with the event.
+      # @param [String] aggregation_key An aggregation key to group events with the same key.
+      # @param [String] priority Priority of the event. Either "normal" (default) or "low".
+      # @param [String] source_type_name The source type of the event.
+      # @param [String] alert_type Either "error", "warning", "info" (default) or "success".
+      # @param [Array, Hash] tags Tags to associate with the event.
+      # @return [String] The correctly formatted service check datagram
+      #
+      # @see https://docs.datadoghq.com/developers/dogstatsd/datagram_shell/#events
+      def _e(title, text, timestamp: nil, hostname: nil, aggregation_key: nil, priority: nil,
+        source_type_name: nil, alert_type: nil, tags: nil)
+
+        raise NotImplementedError
+      end
+
+      # Constructs a service check datagram.
+      #
+      # @param [String] name Name of the service
+      # @param [Symbol] status Either `:ok`, `:warning`, `:critical` or `:unknown`
+      # @param [Time] timestamp The moment when the service was checked. If not provided,
+      #   Datadog will interpret it as the current timestamp.
+      # @param [String] hostname A hostname to associate with the check.
+      # @param [Array, Hash] tags Tags to associate with the check.
+      # @param [String] message A message describing the current state of the service check.
+      # @return [String] The correctly formatted service check datagram
+      #
+      # @see https://docs.datadoghq.com/developers/dogstatsd/datagram_shell/#service-checks
+      def _sc(name, status, timestamp: nil, hostname: nil, tags: nil, message: nil)
+        raise NotImplementedError
+      end
+
+      SERVICE_CHECK_STATUS_VALUES = { ok: 0, warning: 1, critical: 2, unknown: 3 }.freeze
+      private_constant :SERVICE_CHECK_STATUS_VALUES
+    end
+  end
+end
